1c0cda8d149703a5655ae2711db6bfabd8d885c2
dreamer <wildetangeant@gmail.com>
2019-02-21 16:00:11 -0800

    script working

---
+++ ps/personalsite/static/personalsite/js/script.js
@@ -0,0 +1,43 @@
+function acquireLulz(){
+  setTimeout(() => {
+    const cheese = [1, 1331, 224]
+    console.log(cheese);
+  }, 2000)
+console.log("foood");
+}
+acquireLulz();
+
+// const axios = require('axios')
+
+// const getBreeds = () => {
+//   try {
+//     return axios.get('https://dog.ceo/api/breeds/list/all')
+//   } catch (error) {
+//     console.error(error)
+//   }
+// }
+
+
+// function fetch_exist() {
+//     fetch('https://exist.io/api/1/users/$self/today/')
+//      .then(function(response) {
+//         return response.json();
+//   })
+//   .then(function(myJson) {
+//     console.log(JSON.stringify(myJson));
+//   });
+// }
+
+// const url = 'https://exist.io/api/1/users/$self/today/'
+
+// fetch(url).then(data=>console.log(data));
+
+// function getexist() {
+// fetch('https://exist.io/api/1/users/$self/today/')
+//   .then(function(response) {
+//     return response.json();
+//   })
+//   .then(function(myJson) {
+//     console.log(myJson)
+//   });
+// }
--- ps/ps/settings.py
+++ ps/ps/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    
     'personalsite.apps.PersonalsiteConfig',
     'django.contrib.admin',
     'django.contrib.auth',
